f6cbd3a7452cade3697ab39400511f9aa2945017
Garrag Moad <garragmouad8@gmail.com>
2024-06-12 17:02:25 +0200

    Moad_Un_Seul_Robot_Marche

---
+++ CalibrateInterface.py
@@ -0,0 +1,60 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QLineEdit
+from PyQt6.QtGui import QColor
+import sys
+from martypy import Marty
+
+
+class calibrateWindow(QMainWindow):
+    def __init__(self , marty , interface):
+        super().__init__()
+        self.my_marty=marty
+        self.interface=interface
+        self.setWindowTitle("Interface avec Boutons ColorÃ©s")
+
+        layout = QVBoxLayout()
+        self.robot = QLabel('Enter le numéro du robot:')
+        self.ip_input = QLineEdit()
+
+        # Liste des couleurs et des noms des boutons
+        couleurs = [
+            ("Bleu", "blue"),
+            ("Rouge", "red"),
+            ("Vert", "green"),
+            ("Jaune", "yellow"),
+            ("Blue Marin", "navy"),
+            ("Rose", "pink"),
+            ("Noir","black")
+        ]
+
+
+        for nom, couleur in couleurs:
+            bouton = QPushButton(nom)
+            bouton.setStyleSheet(f"background-color: {couleur};")
+            bouton.clicked.connect(lambda _, clr=(nom): self.bouton_clique(clr))
+            layout.addWidget(bouton)
+
+        # Label pour afficher les informations de la couleur sÃ©lectionnÃ©e
+        self.label_infos = QLabel("Aucune couleur sÃ©lectionnÃ©e")
+        layout.addWidget(self.label_infos)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+    def bouton_clique(self, nom):
+        hex_code =self.my_marty.get_color_sensor_hex("left")
+        infos = f"Couleur sÃ©lectionnÃ©e: {nom}\nCode Hex: {hex_code}"
+        self.label_infos.setText(infos)
+        robot=self.robot.text()
+        self.interface.displayColor(nom,hex_code,robot)
+
+    def envoyer_couleur_a_marty(self, nom, hex_code):
+        
+        print(f"Envoi de la couleur {hex_code} pour {nom} au robot Marty")
+
+
+# if __name__ == "__main__":
+#     app = QApplication([])
+#     main_window = calibrateWindow()
+#     main_window.show()
+#     sys.exit(app.exec())
--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QGridLayout, QSlider, QDialog, QLineEdit
 from PyQt6.QtGui import QPixmap, QIcon
 from martypy import Marty
+from CalibrateInterface import calibrateWindow
 
 class LoginWindow(QDialog):
     def __init__(self):
         super().__init__()
         self.initUI()
-        #mohamede
 
     def initUI(self):
         self.setWindowTitle('Login')
@@ -56,16 +56,19 @@ class RobotInterface(QWidget):
     def __init__(self, ip_address):
         super().__init__()
         self.CouleursList = {
-        'Blue': '#0000FF',          # Bleu
+        'Bleu': '#0000FF',          # Bleu
         'Rouge': '#FF0000',         # Rouge
         'Vert': '#008000',          # Vert
-        'Jaune': '#FFFF00',         # Jaune
-        'Noir': '#000000',          # Noir
+        'Jaune': '#FFFF00',         # Jaune         # Noir
         'Blue Marin': '#000080',    # Bleu Marine
-        'Rose': '#FFC0CB',          # Rose
+        'Rose': '#FFC0CB',   
+        'Noir':'#000000'       # Rose
     }
-
+        
+        self.couleurCalibrateNom="BLANC"
+        self.valeurCouleurCaibrate="0"
         self.ip_address = ip_address
+        self.RobotCalibrate=0
         print(self.ip_address)
         self.my_marty=Marty("wifi",self.ip_address)
         self.initUI()
@@ -88,6 +91,9 @@ class RobotInterface(QWidget):
         avancer_button = QPushButton("avancer")
         reculer_button = QPushButton("reculer")
         tourner_button = QPushButton("couleurs")
+        calibrer = QPushButton("calibrer couleurs")
+        labyrinte = QPushButton("labyrinte")
+        #verif=QPushButton("Verifier")
 
         avancer_button.setIcon(QIcon('avancer.png'))
         reculer_button.setIcon(QIcon('reculer.png'))
@@ -96,11 +102,18 @@ class RobotInterface(QWidget):
         avancer_button.clicked.connect(self.avancer)
         reculer_button.clicked.connect(self.reculer)
         tourner_button.clicked.connect(self.getCouleurs)
+        calibrer.clicked.connect(self.showCalibrateInterface)
+        labyrinte.clicked.connect(self.detectColor)
+        #verif.clicked.connect(self.detectC)
 
         # Add direction buttons to layout
         control_layout.addWidget(avancer_button, 0, 2)
         control_layout.addWidget(reculer_button, 0, 0)
         control_layout.addWidget(tourner_button, 0, 1)
+        control_layout.addWidget(calibrer)
+        control_layout.addWidget(labyrinte)
+        #control_layout.addWidget(verif)
+
 
         # Action buttons with images
         actions = [
@@ -169,18 +182,136 @@ class RobotInterface(QWidget):
             return self.my_marty.foot_obstacle_sensed('right')
     
     def getCouleur(self):
-        print(self.my_marty.get_color_sensor_hex("left"))
-        print("yoyo")
+        return self.my_marty.get_color_sensor_hex("left")
+
     def getCouleurs(self):
         print(self.CouleursList)
-    
-    def calibrateCouler(self,couleur,valeur):
-       self.CouleursList[couleur]=valeur
-    def hello():
-          #hhhh
-          print("helo")
 
+    def showCalibrateInterface (self):
+        windowcalbirate = calibrateWindow(self.my_marty ,self)
+        windowcalbirate.show()
+
+    def displayColor(self , couleur, valeur, robot):
+          print(couleur,valeur, robot)
+          self.RobotCalibrate=robot
+          self.CouleursList[couleur] = valeur
+
+    def hex_to_rgb(self, hex_color):
+        if hex_color is None:
+            raise ValueError("Invalid color: None")
+        hex_color = hex_color.lstrip('#')
+        return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+    def rgb_to_hex(self, rgb_color):
+        return '#{:02x}{:02x}{:02x}'.format(*rgb_color)
+
+    def clamp(self, value, min_value=0, max_value=255):
+        return max(min_value, min(value, max_value))
+
+    def create_color_interval(self, hex_color, delta):
+        r, g, b = self.hex_to_rgb(hex_color)
         
+        r_min = self.clamp(r - delta)
+        r_max = self.clamp(r + delta)
+        g_min = self.clamp(g - delta)
+        g_max = self.clamp(g + delta)
+        b_min = self.clamp(b - delta)
+        b_max = self.clamp(b + delta)
+        
+        min_color = self.rgb_to_hex((r_min, g_min, b_min))
+        max_color = self.rgb_to_hex((r_max, g_max, b_max))
+        
+        return (min_color, max_color)
+
+    def create_intervals(self):
+        intervals = {}
+        for color_name, hex_color in self.CouleursList.items():
+                 intervals[color_name] = self.create_color_interval(hex_color, 10)
+        return intervals
+
+    def hex_in_interval(self, hex_color, interval):
+        rgb = self.hex_to_rgb(hex_color)
+        min_rgb = self.hex_to_rgb(interval[0])
+        max_rgb = self.hex_to_rgb(interval[1])
+        return all(min_rgb[i] <= rgb[i] <= max_rgb[i] for i in range(3))
+
+    def color_in_intervals(self, hex_color, intervals):
+        for color_name, interval in intervals.items():
+            if self.hex_in_interval(hex_color, interval):
+                return color_name
+        return 'Jaune'
+
+    def detectColor(self):
+        intervals = self.create_intervals()
+        #for color_name, interval in intervals.items():
+            #print(f"Interval for {color_name}: {interval}")
+        
+        directions = []
+        self.my_marty.stand_straight(1000, None)
+        couleur = self.getCouleur()
+        print("Couleur détectée",couleur)
+        if couleur is None:
+            raise ValueError("getCouleur returned None")
+        detected_color = self.color_in_intervals(couleur, intervals)
+        directions.append(detected_color)
+
+        for _ in range(2):
+            self.my_marty.walk(6, 'auto', 0, 35, 1500, None)
+            self.my_marty.stand_straight(1000, None)
+            couleur = self.getCouleur()
+            print("Couleur détectée",couleur)
+            if couleur is None:
+                raise ValueError("getCouleur returned None")
+            detected_color = self.color_in_intervals(couleur, intervals)
+            directions.append(detected_color)
+
+        self.my_marty.stand_straight(1000, None)
+        self.my_marty.sidestep('right', 6, 35, 1000, None)
+        couleur = self.getCouleur()
+        print("Couleur détectée",couleur)
+        if couleur is None:
+            raise ValueError("getCouleur returned None")
+        detected_color = self.color_in_intervals(couleur, intervals)
+        directions.append(detected_color)
+
+        for _ in range(2):
+            self.my_marty.stand_straight(2000, None)
+            self.my_marty.walk(6, 'auto', 0, -35, 1500, None)
+            couleur = self.getCouleur()
+            print("Couleur détectée",couleur)
+            if couleur is None:
+                raise ValueError("getCouleur returned None")
+            detected_color = self.color_in_intervals(couleur, intervals)
+            directions.append(detected_color)
+
+        self.my_marty.stand_straight(2000, None)
+        self.my_marty.sidestep('right', 6, 35, 1000, None)
+        couleur = self.getCouleur()
+        print("Couleur détectée",couleur)
+        if couleur is None:
+            raise ValueError("getCouleur returned None")
+        detected_color = self.color_in_intervals(couleur, intervals)
+        directions.append(detected_color)
+
+        for _ in range(2):
+            self.my_marty.stand_straight(2000, None)
+            self.my_marty.walk(6, 'auto', 0, 35, 1500, None)
+            couleur = self.getCouleur()
+            print("Couleur détectée",couleur)
+            if couleur is None:
+                raise ValueError("getCouleur returned None")
+            detected_color = self.color_in_intervals(couleur, intervals)
+            directions.append(detected_color)
+            self.my_marty.stand_straight(1000, None)
+
+        print("chemin fini")
+        print(directions)
+
+    #def detectC(self):
+        #intervals = self.create_intervals()
+        #couleur = self.getCouleur()
+        #detected_color = self.color_in_intervals(couleur, intervals)
+        #print("Couleur détectée",detected_color)
 
 def main():
     app = QApplication(sys.argv)
